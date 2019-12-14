fd6796affca82eb9659be3aafafeef22d230e2e1
sriyegna <sriyegna@users.noreply.github.com>
2019-12-14 14:14:57 -0500

    Initial Commit

---
+++ Website/Verifit/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './auth/auth.guard';
+
+
+const routes: Routes = [
+  {path:'', redirectTo:'/user/login', pathMatch:'full'},
+  {path:'user', component:UserComponent, 
+    children: [
+      {path:'registration', component:RegistrationComponent}, // /user/register
+      {path:'login', component:LoginComponent} // /user/login
+    ]
+  },
+  {path:'home', component:HomeComponent, canActivate:[AuthGuard]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
